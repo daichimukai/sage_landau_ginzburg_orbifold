4bbbe9fdf35d0a04625584e0a171e66a946f4726
Daichi Mukai <mukai.daichi.37e@kyoto-u.jp>
2017-01-20 21:37:08 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name         = 'landau_ginzburg_orbifold',
+      version      = '0.1',
+      description  = 'Landau-Ginzburg orbifold',
+      url          = 'https://github.com/ici-ya/sage_landau_ginzburg_orbifold/',
+      author       = 'Daichi Mukai',
+      author_email = 'ddmukai@gmail.com',
+      license      = 'GPLv3',
+      packages     = ['landau_ginzburg_orbifold'],
+      zip_safe     = False)
