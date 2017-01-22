040683ebe7ccc71534ed21589ae802803c150dfb
Daichi Mukai <mukai.daichi.37e@kyoto-u.jp>
2017-01-22 21:42:13 +0900

    Make the 'centralizer' method to be a cached one
    * Add docstring

--- landau_ginzburg_orbifold/group_patch.py
+++ landau_ginzburg_orbifold/group_patch.py
@@ -1,5 +1,14 @@
-from sage.groups.matrix_gps.finitely_generated  import FinitelyGeneratedMatrixGroup_gap
+r"""
+Patches for Group operations
 
+AUTHORS:
+- Daichi Mukai (2017-01)
+"""
+
+from sage.groups.matrix_gps.finitely_generated  import FinitelyGeneratedMatrixGroup_gap
+from sage.misc.cachefunc import cached_method
+
+@cached_method
 def __centralizer__(self, element):
     g = element.gap()
     C = self.gap().Centralizer(g)
