46a5d8d3ef98984235f3e6432bed8a994dae8e18
Daichi Mukai <mukai.daichi.37e@kyoto-u.jp>
2017-01-24 17:32:44 +0900

    Algorithm to compute eigenvalues of a matrix
    * Intend to use for matrices over Universal Cyclotomic Field

--- landau_ginzburg_orbifold/group_patch.py
+++ landau_ginzburg_orbifold/group_patch.py
@@ -5,8 +5,11 @@ AUTHORS:
 - Daichi Mukai (2017-01)
 """
 
+from sage.arith.misc import lcm
 from sage.groups.matrix_gps.finitely_generated  import FinitelyGeneratedMatrixGroup_gap
 from sage.misc.cachefunc import cached_method
+from sage.rings.integer_ring import ZZ
+from sage.rings.number_field.number_field import CyclotomicField
 
 @cached_method
 def __centralizer__(self, element):
@@ -14,3 +17,18 @@ def __centralizer__(self, element):
     C = self.gap().Centralizer(g)
     return self.subgroup(C.GeneratorsOfGroup())
 FinitelyGeneratedMatrixGroup_gap.centralizer = __centralizer__
+
+def eigenvalues(element):
+    def __multiplicative_order__(element):
+        g = element
+        one = element.parent().one()
+        i = 1
+        while True:
+            if g == one:
+                return ZZ(i)
+            g = g * element
+            i += 1
+
+    order = lcm([value.conductor() for value in element.dict().values()] + [__multiplicative_order__(element)])
+    field = CyclotomicField(order)
+    return element.change_ring(field).eigenvalues()
